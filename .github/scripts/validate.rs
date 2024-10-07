fd77126ae14069fa79ee3bcbac73b3d3331e2d88
warittornc <69615455+warittornc@users.noreply.github.com>
2024-10-07 15:44:04 +0700

    [feat] Add Validate CI/CD (#2)
    * add script
    
    * minor fix
    
    * generate registry
    
    * add registry validator project, add binary
    
    * add placeholder for bothan-core version
    
    * convert script
    
    * add github action
    
    * cleanup
    
    * aggregated signals
    
    * add directory check
    
    * change name for clarity
    
    * aggregated signals
    
    * remove outer registry
    
    ---------
    
    Co-authored-by: colmazia <ongart.pisan@gmail.com>
    Co-authored-by: warittornc <warittornc@users.noreply.github.com>

---
+++ .github/scripts/validate.rs
@@ -0,0 +1,31 @@
+#!/usr/bin/env rust-script
+//! ```cargo
+//! [dependencies]
+//! serde_json = "1.0.128"
+//! bothan-core = "0.0.1-alpha.3"
+//! ```
+
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+use bothan_core::registry::{Invalid, Registry};
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        eprintln!("unexpected arguments: {:?}", args);
+        std::process::exit(1);
+    }
+
+    let path = Path::new(args[1].as_str());
+    let registry: Registry<Invalid> = {
+        let file = File::open(path).expect("Failed to open registry file");
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader).expect("Failed to parse registry file")
+    };
+    registry.validate().expect("Registry is invalid");
+
+    println!("Registry is valid");
+}
